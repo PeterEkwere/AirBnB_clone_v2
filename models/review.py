5039e995e413882b17d66ae41e39b34516a4db03
PeterEkwere <peterekwere04@gmail.com>
2023-09-20 17:00:07 -0700

    refactored review file

--- models/review.py
+++ models/review.py
@@ -5,11 +5,18 @@ from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 from models.place import Place
 from models.user import User
+import os
 
+environment = os.environ.get("HBNB_TYPE_STORAGE")
 
 class Review(BaseModel, Base):
     """ Review classto store review information """
     __tablename__ = "reviews"
-    place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
-    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-    text = Column(String(1024), nullable=False)
+    if environment == "db":
+        place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+        text = Column(String(1024), nullable=False)
+    else:
+        place_id = ""
+        user_id = ""
+        text = ""
