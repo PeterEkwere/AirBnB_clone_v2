e71f20e3489709e14a90ba4e1d5637bf585a5ba3
PeterEkwere <peterekwere007@gmail.com>
2023-10-04 17:20:25 +0000

    initial commit for task 0

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# This script creates a new ubuntu machine with all confi ready for deployment to server
+sudo apt-get -y update
+sudo apt-get -y install nginx
+sudo mkdir -p /data/web_static/releases/test/
+sudo touch /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test/  /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+sudo sh -c 'cat sed/file.html > /data/web_static/current/index.html'
+sudo sh -c 'echo "Hello World!" > /var/www/html/index.nginx-debian.html'
+sudo touch /etc/nginx/html/404.html
+sudo sh -c 'echo "Ceci n'\''est pas une page" > /etc/nginx/html/404.html'
+sudo sed -i -f sed/insert.sed /etc/nginx/sites-available/default
+sudo sed -i -f sed/404.sed /etc/nginx/sites-available/default
+sudo sed -i -f sed/alias.sed /etc/nginx/sites-available/default
+sudo sed -i -f sed/head.sed /etc/nginx/sites-available/default
+sudo service nginx restart
---
+++ sed/alias.sed
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+/server_name _;/a\
+	location /hbnb_static/ {\
+	    alias /data/web_static/current/;\
+	}
