73a81466bb0b4d328e3c626635b454443f75bf0b
PeterEkwere <peterekwere007@gmail.com>
2023-10-04 17:58:56 +0000

    added shared file

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,6 +3,7 @@
 sudo apt-get -y update
 sudo apt-get -y install nginx
 sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
 sudo touch /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/  /data/web_static/current
 sudo chown -R ubuntu /data/
