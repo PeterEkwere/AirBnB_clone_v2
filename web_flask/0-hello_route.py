87a8058dc0fd95fed075c4e8e62767b3b5a804be
PeterEkwere <peterekwere04@gmail.com>
2023-10-20 05:41:01 -0700

    initla commit for task 0

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+    This is a flask web app
+    Author: Peter Ekwere
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def Hello():
+    """ This function displays Hello HBNB! """
+    return "Hello HBNB!"
+
+
+if __name__ == '__main__':
+    """ This will trigger when run directly """
+    app.run(host='0.0.0.0', port=5000)
