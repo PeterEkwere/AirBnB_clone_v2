be79d08eabb7b23a9c6d4bc31e7cbed4079d2958
PeterEkwere <peterekwere04@gmail.com>
2023-10-20 12:53:42 -0700

    intia; commit for task 1 and 2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+    This is a flask web app
+    Author: Peter Ekwere
+"""
+from flask import Flask
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def Hello():
+    """ This function displays Hello HBNB! """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def holberton():
+    """ This function displays HBNB """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def path_text(text):
+    """ this function concatenates and prints the path """
+    return f"C {escape(text.replace('_', ' '))}"
+
+
+if __name__ == '__main__':
+    """ This will trigger when run directly """
+    app.run(host='0.0.0.0', port=5000)
